ef97ed71c0c4927df647306ee4529cc4c344dd5b
JaysonWaters <48612339+JaysonWaters@users.noreply.github.com>
2022-12-02 21:01:42 -0500

    12/2/2022(1)

---
+++ WebScraper.py
@@ -0,0 +1,16 @@
+'''
+Python Webscraper using https://www.geeksforgeeks.org/python-web-scraping-tutorial/
+Author: Jayson Waters
+Date: 12/2/2022
+'''
+import requests
+
+# Making a GET request
+r = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+ 
+# check status code for response received
+# success code - 200
+print(r)
+ 
+# print content of request
+print(r.content)
